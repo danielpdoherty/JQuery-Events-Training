f22847d2cbaed21adeeb6a7ebc207896476770f1
Dan <you@example.com>
2017-03-09 09:17:44 -0600

    on click event

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -2,4 +2,12 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
+$('#imperatives span').on('click', function(){
+	var now = moment().format("dddd, MMMM Do, YYYY, h:mm:ss A");
+
+
+	$('ul').append('<li>Clicked:' +$(this).text()+  ' at ' +$('#date').append(now)+  '</li>');
+
+})
+
 })
