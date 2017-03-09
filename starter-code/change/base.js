67151fafa0bc0072eb151f76e4d9c75f4a7597c8
Dan <you@example.com>
2017-03-09 09:01:11 -0600

    change int

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
-})
+  // $('.#total').on('click', function(){
+  // 	var left = $('#left').val();
+  // 	var right = $('#right').val();
++  
+$('#total').on('click',function(){
+ +  var left = parseInt($('#left').val());
+ +  var right = parseInt($('#right').val());
+ +   
+ 	var total = left + right;
+ +    
+
+ 	$('#total').val(total);
+
+// })
